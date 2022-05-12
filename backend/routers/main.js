ca08ae6fd5c7091e04eb7e5487fb6b390e204f53
dany98sa <demarcodaniele98@gmail.com>
2022-05-12 09:22:24 +0200

    rework project

--- backend/routers/main.js
+++
@@ -1,6 +0,0 @@
-const mainRoute = require('express').Router()
-const project = require('./project')
-
-mainRoute.use('/projects', project)
-
-module.exports  = mainRoute
--- backend/utility/validationResultCustom.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require("express-validator");
-
-module.exports = validationResultCustom=validationResult.withDefaults({
-    formatter: error=>{
-        return{
-            messagge: error.param+" "+error.value+" "+error.msg
-        }
-    }
-})
