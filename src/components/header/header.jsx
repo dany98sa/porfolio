e7b57628079616e6882adb12b197b0dafafe5357
dany98sa <demarcodaniele98@gmail.com>
2022-05-12 11:44:57 +0200

    create portfolio style

---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+function Header() {
+    return(
+        <div className="container header-container">
+            <h3>
+                <p>
+                    I <span>'</span> am
+                </p>
+                Daniele
+                <h2>
+                    FullStack-Developer
+                </h2>
+            </h3>
+        </div>
+    )
+}
+
+export default Header;
